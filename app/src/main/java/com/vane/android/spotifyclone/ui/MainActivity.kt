1b4a87007ea6ebf86505dab2af18047269ef6020
ivVane <iv.vane@gmail.com>
2021-06-19 00:05:23 +0200

    Implemented Dagger Hilt di

--- app/src/main/java/com/vane/android/spotifyclone/ui/MainActivity.kt
+++ app/src/main/java/com/vane/android/spotifyclone/ui/MainActivity.kt
@@ -2,9 +2,16 @@ package com.vane.android.spotifyclone.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.RequestManager
 import com.vane.android.spotifyclone.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    // Just for testing
+    lateinit var glide: RequestManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
