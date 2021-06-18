912633d1ff666194dfe0c60650a83210c843debd
ivVane <iv.vane@gmail.com>
2021-06-18 20:36:34 +0200

    Adding dependencies, drawables files, configuring xml layouts

--- app/src/main/java/com/vane/android/spotifyclone/ui/MainActivity.kt
+++ app/src/main/java/com/vane/android/spotifyclone/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.vane.android.spotifyclone
+package com.vane.android.spotifyclone.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.vane.android.spotifyclone.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
